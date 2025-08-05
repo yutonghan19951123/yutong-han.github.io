d4ec2fcd93ad5fee7ce54d973052ba732354b6a0
yutonghan <yutonghan@yutonghandeMacBook-Pro.local>
2025-08-05 01:00:43 -0700

    Fix defineOgImage error in projects slug page

--- app/pages/projects/[...slug].vue
+++ app/pages/projects/[...slug].vue
@@ -17,17 +17,7 @@ const { data: surround } = await useAsyncData(`${route.path}-surround`, () =>
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation', ref([]))
 const blogNavigation = computed(() => navigation.value.find(item => item.path === '/blog')?.children || [])
 
-const breadcrumb = computed(() => mapContentNavigation(findPageBreadcrumb(blogNavigation?.value, page.value)).map(({ icon, ...link }) => link))
-
-if (page.value.image) {
-  defineOgImage({ url: page.value.image })
-} else {
-  defineOgImageComponent('Blog', {
-    headline: breadcrumb.value.map(item => item.label).join(' > ')
-  }, {
-    fonts: ['Geist:400', 'Geist:600']
-  })
-}
+const _breadcrumb = computed(() => mapContentNavigation(findPageBreadcrumb(blogNavigation?.value, page.value)).map(({ icon, ...link }) => link))
 
 const title = page.value?.seo?.title || page.value?.title
 const description = page.value?.seo?.description || page.value?.description
